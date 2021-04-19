50cab6f0dd880f49457c8d0c907182fa536d09aa
Aleksvin8888 <aleksvin8888@gmail.com>
2021-04-19 23:37:26 +0300

    add editRolesRequest

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -79,7 +79,7 @@
                                         </button>
                                     </div>
                                     <div class="modal-body" style="color:black">
-                                        Удаляем?
+                                        Удаляем?  Id  -  {{ $user->id }}
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
