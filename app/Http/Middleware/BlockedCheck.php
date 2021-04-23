dc47ad3c719b697ccd06067e8abb5ee8c041d17e
AnnaBorisenko <anutka.kiska.87@gmail.com>
2021-04-23 19:17:27 +0300

    Created middlewares for blocked and admin users, logout blocked user

--- app/Http/Middleware/BlockedCheck.php
+++ app/Http/Middleware/BlockedCheck.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BlockedCheck
 {
@@ -20,6 +21,12 @@ class BlockedCheck
         if ($request->user()->is_blocked =='0') {
         return $next($request);
         }
+
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
 
         return redirect('/');
     }
