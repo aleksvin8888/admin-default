b2b6f86ed6b4ed5af50a7edfff44723002b135de
AnnaBorisenko <anutka.kiska.87@gmail.com>
2021-05-05 11:18:23 +0300

    done click on dark field

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@include('admin.includes.header')
+    <title>@yield('title')</title>
+    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet"/>
+
+</head>
+
 <body class="sb-nav-fixed">
 @include('admin.includes.topNavbar')
 <div id="layoutSidenav">
@@ -16,7 +30,6 @@
        @include('admin.includes.footer')
     </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
 <script src="{{ asset('assets/js/app.js') }}"></script>
 </body>
 </html>
