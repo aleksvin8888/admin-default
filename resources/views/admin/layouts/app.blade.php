4b92ae806bcd7a4b692748c042d64f4e462a1496
Anatoliy Prozhoga <tolya.prozhoga@gmail.com>
2021-05-05 15:07:18 +0300

    Added graphics to the dashboard page and DataTables

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,5 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@include('admin.includes.header')
+    <title>@yield('title')</title>
+    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"
+            crossorigin="anonymous"></script>
+</head>
+
 <body class="sb-nav-fixed">
 @include('admin.includes.topNavbar')
 <div id="layoutSidenav">
